462dabca7296da6c354d445c1123d19791c2f1cf
haticesahin <haticesahi14@gmail.com>
2020-06-03 22:19:30 +0300

    saat game kismi eklendi

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject saatPanel, turPanel, zamanPanel;
+
+    public void SaatPanelOpen()
+    {
+        if (GameObject.FindGameObjectWithTag("SaatBtn"))
+        {
+            saatPanel.SetActive(true);
+        }
+    }
+
+    public void TurPanelOpen()
+    {
+        if (GameObject.FindGameObjectWithTag("Button"))
+        {
+            turPanel.SetActive(true);
+        }
+    }
+
+    public void ZamanPanelOpen()
+    {
+        if (GameObject.FindGameObjectWithTag("Button"))
+        {
+            zamanPanel.SetActive(true);
+        }
+    }
+
+    public void SaatDersSceneOpen()
+    {
+        SceneManager.LoadScene("SaatDersScene");
+    }
+
+    public void SaatGameSceneOpen()
+    {
+        SceneManager.LoadScene("SaatScene");
+    }
+
+    public void TurSceneOpen()
+    {
+        SceneManager.LoadScene("TurScene");
+    }
+
+    public void ZamanSceneOpen()
+    {
+        SceneManager.LoadScene("ZamanScene");
+    }
+
+    public void GameSceneOpen()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
---
+++ Assets/Scripts/StartMenuManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class StartMenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject startBtn, exitBtn;
+    void Start()
+    {
+        FadeOut();
+    }
+
+    void FadeOut()
+    {
+        startBtn.GetComponent<CanvasGroup>().DOFade(1, 0.8f);
+        exitBtn.GetComponent<CanvasGroup>().DOFade(1, 0.8f);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SecenekScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
